fc966814eb973c35ab1d028cc5dc5e97fffbeffc
Thor <thor@10146.local>
2019-09-25 12:19:03 +0200

    opgaver uge 39

--- HangMan.java
+++ HangMan.java
@@ -8,12 +8,20 @@
 * med koden. Bla er teksten taget helt random fra nettet så overvej om I vil bruge en anden tekst i stedet.
 * Løs derpå flg tasks:
 *
-* 1) Sørg for at SecretWord rummer mindst 4 karakterer
+* 1) Sørg for at SecretWord rummer mindst 4 karakterer (så ArrayHelperHangMan-metoden skal tjekke
+* for ordlængde inden ordet returneres)
 * 2) Sørg for at tjekket for om en karaktér er i ordet også tjekker 
-* hvor mange gange karakteren forekommer.
+* hvor i ordet karakteren forekommer og hvor mange gange . 
+* Lad tjekket være i ArrayHelperHangman så man i main får dette retur: 
+* [1, 2, -1, -1, -1] = ArrayHelperHangMan.getAllIdx("eller",'l');
+*
 * 3) Ordet som brugeren bygger med de rigtigt gættede karakterer - myWord - bruges slet 
 * ikke i koden så man kan ikke se hvor langt man er. Modificér koden så dér, hvor der
 * bare udskrives "is found at" tilføjes karakteren til myWord som så udskrives.
+* Altså flg udskrives hvis man har gættet 'l' i ordet "eller":
+* W: [null l, l, null, null]
+* Sørg også for at while-loopet stopper hvis man har gættet alle karakterer. 
+*
 * 4) De forkerte bogstaver gemmes ikke. Overvej at sende dem med som parameter
 * til figuren og print dem som en ekstra linje i bunden.
 *
