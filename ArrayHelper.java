8d7b3c77feae52fc82f6ff2d6d3feec2ffc60142
Thor <thor@10146.local>
2019-09-25 09:40:48 +0200

    opgaver uge 39

--- ArrayHelper.java
+++ ArrayHelper.java
@@ -2,5 +2,6 @@ import java.util.Random;
 
 public class ArrayHelper {
   // TODO:
-  // Skriv metoden som fylder et array med tilfældige tal
+  // Skriv metoden som fylder et array med tilfældige tal. Erklær metoden for statisk. 
+  // Så kan den bruges direkte i main uden at man skulle instantiere et objekt
 }
